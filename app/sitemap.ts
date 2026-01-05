02a0a8e7d416c9e19098e19c23761b0f90b9b10a
pradhys <cognizant.sharma@mercedes-benz.com>
2026-01-05 16:45:39 +0530

    gsc fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,16 @@
 import { CATEGORIES } from "@/constants";
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://pickuplineshub.vercel.app";
   const categories = Object.values(CATEGORIES);
 
   const urls = categories.map((item) => ({
     url: `${baseUrl}/${item}-pickup-lines/`,
-    lastModified: new Date("2026-01-05"),
+    lastModified: "2026-01-05",
   }));
 
-  return [{ url: baseUrl, lastModified: new Date("2026-01-05") }, ...urls];
+  return [{ url: baseUrl, lastModified: "2026-01-05" }, ...urls];
 }
